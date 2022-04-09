ec09290002ddd80050467fe2a441513f9dcfef51
amarnathp-o2o <amarnathp.official@gmail.com>
2022-04-09 11:31:21 +0530

    fixed some bugs

--- libs/mapper.js
+++ libs/mapper.js
@@ -1,14 +1,14 @@
 const IonJs =  {
   config: function (param) {
-    var map = JSON.stringify(param.map);
-    Object.keys(map).forEach(key => {
-      map["@" + key] = map[key];
-      if (!!map.baseURL) map["@" + key] = map.baseURL + map[key];
+    var map = param.map;
+    Object.entries(map).forEach(([key, value]) => {
+      map["@" + key] = value;
       delete map[key];
+      if (!!param.baseURL) map["@" + key] = param.baseURL + value;
     });
     var ionMapJs = document.createElement('script');
     ionMapJs.type = 'importmap';
-    ionMapJs.innerHTML = '{"imports":' + map + "}";
+    ionMapJs.innerHTML = '{"imports":' + JSON.stringify(map) + "}";
     document.head.appendChild(ionMapJs);
   }
 };
